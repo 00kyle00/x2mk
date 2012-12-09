1377e5657dfcc8faef07af7edc8d9a9b5484ddf9
kyle <kyle@badmail.com>
2012-12-09 22:08:09 +0100

    Minor tweaks to scripts.

--- directinput/controller.lua
+++ directinput/controller.lua
@@ -1,6 +1,10 @@
 require "constants"
 require "utilities"
 
+Kbd = Keyboard
+Mse = Mouse
+Ctl = Controller
+
 -- Function is invoked whenever state of controller button is changed
 function button_changed(btn)
   local btn_state = Controller.button(btn)
@@ -12,11 +16,12 @@ function button_changed(btn)
   elseif btn == CB_BCK then Keyboard.press_release(VK_ESCAPE, btn_state)
   elseif btn == CB_X then Mouse.press_release(MB_M, btn_state)
   elseif btn == CB_Y then Keyboard.press_release(VK_BROWSER_REFRESH, btn_state)
-  elseif btn == CB_GUIDE then Controller.rumble(value, value)
+  --elseif btn == CB_GUIDE then Controller.rumble(value, value)
   elseif btn == CB_UP then Keyboard.press_release(VK_UP, btn_state)
   elseif btn == CB_DOWN then Keyboard.press_release(VK_DOWN, btn_state)
   elseif btn == CB_LEFT then Keyboard.press_release(VK_LEFT, btn_state)
   elseif btn == CB_RIGHT then Keyboard.press_release(VK_RIGHT, btn_state)
+  elseif btn == CB_STA then Keyboard.press_release(VK_SPACE, btn_state)
   end
 
   -- execute only on press
@@ -30,8 +35,8 @@ end
 -- tick function is called every ~10ms. It should contain all logic translating
 -- controller state to keyboard/mouse stuff. 
 function tick()
-  Mouse.move(Controller.stick(0), 0.15, 5);
-  Mouse.move(Controller.stick(1), 0.15, 15);
+  Mouse.move(Controller.stick(0), 0.15, 3);
+  Mouse.move(Controller.stick(1), 0.15, 12);
   Mouse.wheel(Controller.trigger(0) - Controller.trigger(1), 0.01, 50)
   --Controller.rumble(Controller.trigger(0), Controller.trigger(1))
 end
